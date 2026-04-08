604623570b71fb6012b71d81adca80badf67c476
kyle <kyletapac4@gmail.com>
2026-04-08 14:32:41 +0800

    update logout

--- EcoT/process/logout.php
+++ EcoT/process/logout.php
@@ -1,13 +1,8 @@
 <?php
 session_start();
-
-// Unset all session variables
-$_SESSION = array();
-
-// Destroy the session
+session_unset();
 session_destroy();
 
-// Redirect to login page
-header("Location: ../pages/login.php");
+header("Location: ../index.php");
 exit();
-?> 
+?>
